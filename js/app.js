197d202cd7a9fc42d12cf70c090599feb7857907
Brian Workman <bworkm@outlook.com>
2016-12-19 12:57:08 -0800

    randomizer functions working. Plots and Ships constructors complete.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 var allShips = [];
 var allPlots = [];
+var xPosArr = ['A', 'B', 'C', 'D' ,'E'];
+var maxHeight = 5;
+var maxWidth = 5;
 
 function Ship(name, size) {
   this.name = name;
@@ -11,25 +14,52 @@ function Ship(name, size) {
   this.updateSunk = function() {
 
   };
-  this.push(allShips);
+  allShips.push(this);
 }
 
 function Plot(x, y) {
   this.x = x;
   this.y = y;
   this.occupied = false;
-  this.updateOccupied = function() {
-
-  };
-  this.push(allPlots);
+  this.updateOccupied = function() {};
+  allPlots.push(this);
 }
 
+new Ship('USS Test', 2);
 //*********************************
 // Functions for Placing Ships
 
-Randomize for X
-Randomize for Y
-Randomize for Direction (1 thru 4)
-Check for nearby open spaces X
-Check for nearby open spaces Y
-seatsTaken() for updating occupied spots
+// Randomize for X
+function randomizeForX() {
+  var xPos = (Math.floor(Math.random() * maxWidth) + 1);
+  console.log(xPos,'X value');
+}
+
+// Randomize for Y
+function randomizeForY() {
+  var yPos = (Math.floor(Math.random() * maxHeight) + 1);
+  console.log(yPos,'Y value');
+}
+
+// Randomize for Direction (1 thru 4)
+function randomDirection() {
+  var direction = (Math.floor(Math.random() * 4) + 1);  //Randomizes 1 thru 4
+  console.log(direction,'Direction value');
+  return direction;
+}
+randomDirection();
+// Check for nearby open spaces X
+// Check for nearby open spaces Y
+// seatsTaken() for updating occupied spots
+
+function createPlots() {
+
+  for (var i = 0; i < xPosArr.length; i++) {
+    for (var j = 0; j < maxWidth; j++) {
+      new Plot(xPosArr[i], j);
+    }
+  }
+}
+
+// function calls
+createPlots();
