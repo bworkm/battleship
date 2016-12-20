69fd6b8b9579f5cf0782e38f40e053b697f6e490
Lord Chika Onyebueke <onye108@gmail.com>
2016-12-19 21:35:55 -0800

    designed and linked to index.html

---
+++ Login.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//*************************USER INPUT****************************
+var formData = document.getElementById('userinput');
+
+formData.addEventListener('submit', inputNewUserData);
+
+function inputNewUserData (event) {
+  event.preventDefault();
+
+
+//****************************Difficulty*****************************
