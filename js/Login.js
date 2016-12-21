c2330a0ed6c28f04d05c7334ee9912bf52731f30
juanchaves <chavesjuanm@gmail.com>
2016-12-21 13:19:30 -0800

    added local storage for user name at index and get user name at app using getItem and JSON stringify

--- js/Login.js
+++ js/Login.js
@@ -1,15 +1,24 @@
 'use strict';
 
+
 //*************************USER INPUT****************************
 var formData = document.getElementById('userinput');
 
 formData.addEventListener('submit', inputNewUserData);
 
 function inputNewUserData (event) {
-  var userName =
-  event.preventDefault();}
+  event.preventDefault();
+  var userName = event.target.name.value;
+  localStorage.setItem('user_name',JSON.stringify(userName));
+  console.log(localStorage.user_name);
+  goToPage ();
+}
+
+function goToPage () {
+  location.href='game.html';
+}
 //******************************music loop*******************************
-var audio = document.getElementById("myMusic");
+var audio = document.getElementById('myMusic');
 audio.autoplay = true;
 audio.load();
 //****************************Difficulty***************************
